836451e799262184fb0d2a357ec9982d9e82dd80
MarianoCarrizo <Mariano.Carrizo4280@gmail.com>
2020-11-08 20:19:11 -0300

    Consulta

---
+++ test/ArbolGeneral.cs
@@ -0,0 +1,132 @@
+using System;
+using System.Collections.Generic;
+
+namespace DeepSpace
+{
+	public class ArbolGeneral<T>
+	{
+		
+		private T dato;
+		private List<ArbolGeneral<T>> hijos = new List<ArbolGeneral<T>>();
+
+		public ArbolGeneral(T dato) {
+			this.dato = dato;
+		}
+	
+		public T getDatoRaiz() {
+			return this.dato;
+		}
+	
+		public List<ArbolGeneral<T>> getHijos() {
+			return hijos;
+		}
+	
+		public void agregarHijo(ArbolGeneral<T> hijo) {
+			this.getHijos().Add(hijo);
+		}
+	
+		public void eliminarHijo(ArbolGeneral<T> hijo) {
+			this.getHijos().Remove(hijo);
+		}
+	
+		public bool esHoja() {
+			return this.getHijos().Count == 0;
+		}
+	
+		public int altura() {
+			int height = 0;
+			if (this.esHoja())
+			{
+				return height;
+			}
+			foreach (var h in this.getHijos())
+				if (h.altura() > height)
+				{
+					height = h.altura();
+
+				}
+			return 1 + height;
+		}
+
+
+		public int nivel(T dato) {
+			Cola<ArbolGeneral<T>> C = new Cola<ArbolGeneral<T>>();
+			ArbolGeneral<T> auxi;
+			int nivel = 0;
+			C.encolar(this);
+			C.encolar(null);
+			while (!C.esVacia())
+			{
+				auxi = C.desencolar();
+				
+				if (dato.Equals(auxi) == true)
+				{
+					return nivel;
+				}
+				if (auxi == null)
+				{
+					nivel++;
+					if (!C.esVacia())
+					{
+						C.encolar(null);
+					}
+				}
+				else
+				{
+					foreach (var hijo in auxi.getHijos())
+					{
+						C.encolar(hijo);
+					}
+				}
+
+			}
+			return nivel;
+
+
+		}
+
+		public void Porniveles()
+		{
+
+			Cola<ArbolGeneral<T>> c = new Cola<ArbolGeneral<T>>();
+			ArbolGeneral<T> arbolaux;
+
+			c.encolar(this);
+
+            while (!c.esVacia())
+			{
+				arbolaux = c.desencolar();
+
+				Console.Write(arbolaux.getDatoRaiz() + "  ");
+
+
+				foreach (var hijo in arbolaux.getHijos()) 
+					c.encolar(hijo);
+
+				
+			
+			
+			}
+
+		
+		
+		
+		}
+		public void PostOrden()
+		{
+			foreach (var hijo in this.getHijos())
+				hijo.PostOrden();
+
+			Console.WriteLine(this.getDatoRaiz() + "  ");
+
+
+
+		}
+		
+
+		
+
+
+
+	}
+}
---
+++ test/Estrategia.cs
@@ -0,0 +1,143 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.Remoting.Messaging;
+
+namespace DeepSpace
+{
+
+	class Estrategia
+	{
+
+
+		public String Consulta1(ArbolGeneral<Planeta> arbol)
+		{
+			int d = 0;
+
+			Cola<ArbolGeneral<Planeta>> c = new Cola<ArbolGeneral<Planeta>>();
+			int Conta = 0;
+
+			c.encolar(arbol);
+
+			while (!c.esVacia())
+			{
+				arbol = c.desencolar();
+
+				if (arbol.getDatoRaiz().team == 2)
+				{
+					d = arbol.nivel(arbol.getDatoRaiz());
+					break;
+
+
+
+				}
+				Conta++;
+
+				Console.Write(arbol.getDatoRaiz() + "  ");
+
+
+				foreach (var hijo in arbol.getHijos())
+					c.encolar(hijo);
+
+
+
+
+
+			}
+
+
+
+
+
+
+			// calcular y retornar un texto con distancia entre raiz y nodo del bot
+			return "la distancia entre la raiz y del bot es de : " + d;
+		}
+
+
+		public String Consulta2(ArbolGeneral<Planeta> arbol)
+		{
+			Cola<ArbolGeneral<Planeta>> c = new Cola<ArbolGeneral<Planeta>>();
+			int Conta = 0;
+
+			c.encolar(arbol);
+
+			while (!c.esVacia())
+			{
+				arbol = c.desencolar();
+
+				if (arbol.getDatoRaiz().population > 10)
+				{
+					Conta++;
+				}
+
+				Console.Write(arbol.getDatoRaiz() + "  ");
+
+
+				foreach (var hijo in arbol.getHijos())
+					c.encolar(hijo);
+
+
+
+
+			}
+
+
+
+			//calcular cantidad de planetas con población > 10 en cada nivel del arbol (recorrido por niveles,con limitante de población)
+
+			return "La cantidad de planetas con población mayor a 10 son : " + Conta + "  ";
+		}
+
+
+		public String Consulta3(ArbolGeneral<Planeta> arbol)
+		{
+			// calcular  promedio poblacion por nivel de arbol (recorrido por niveles, sumar en total y cantidad y dividir por cantidad para sacar promedio)
+
+			Cola<ArbolGeneral<Planeta>> c = new Cola<ArbolGeneral<Planeta>>();
+			long Conta = 0;
+			long canti = 0;
+
+			c.encolar(arbol);
+
+			while (!c.esVacia())
+			{
+				arbol = c.desencolar();
+
+
+
+
+				foreach (var hijo in arbol.getHijos())
+					c.encolar(hijo);
+				Conta += arbol.getDatoRaiz().population;
+				canti++;
+
+
+
+
+			}
+			Double Promedio = Conta / canti;
+			return "el promedio de poblacion por nivel del arbol es de : " + Promedio;
+		}
+
+		public Movimiento CalcularMovimiento(ArbolGeneral<Planeta> arbol)
+		{
+
+
+
+			return null;
+		}
+
+	
+
+
+	}
+}
+			
+
+		
+		
+
+
+	
